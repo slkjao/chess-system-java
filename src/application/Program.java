dd54d7f8cdeb71326f38a51ec96b0830334e92ad
joao costa <joaovaraujoc@gmail.com>
2023-02-01 16:18:56 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main (String[] args) {
-		System.out.println("ola");
+		
+		Position p1 = new Position(1,2);
+		System.out.println(p1.toString());
 	}
 }
